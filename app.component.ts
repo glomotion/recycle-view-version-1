558fa4a33b13bf3a0c43c5e5f83b93f59bc308d8
glomotion <tim@glo.id.au>
2020-08-13 11:47:44 +1000

    2 files modified

--- app.component.ts
+++ app.component.ts
@@ -33,134 +33,14 @@ export class App extends LitElement {
 
   } 
 
-  private getSlidingWindow = isScrollDown => {
-    const increment = this.listSize / 2;
-    let firstIndex;
-    
-    if (isScrollDown) {
-      firstIndex = this.currentIndex + increment;
-    } else {
-      firstIndex = this.currentIndex - increment;
-    }
-    
-    if (firstIndex < 0) {
-      firstIndex = 0;
-    }
-    
-    return firstIndex;
-  }
-
-  private recycleDOM = firstIndex => {
-    for (let i = 0; i < this.listSize; i++) {
-      const tile = document.querySelector("#tile-" + i);
-      tile.firstElementChild.innerText = collection[i + firstIndex].title;
-      tile.lastChild.setAttribute("src", collection[i + firstIndex].imgSrc);
-    }
-  }
-
-  private getNumFromStyle = numStr => Number(numStr.substring(0, numStr.length - 2));
-
-  private adjustPaddings = isScrollDown => {
-    const container = this.shadowRoot.querySelector(".list") as HTMLElement;
-    const currentPaddingTop = this.getNumFromStyle(container.style.paddingTop);
-    const currentPaddingBottom = this.getNumFromStyle(container.style.paddingBottom);
-    const remPaddingsVal = 170 * (listSize / 2);
-    if (isScrollDown) {
-      container.style.paddingTop = currentPaddingTop + remPaddingsVal + "px";
-      container.style.paddingBottom = currentPaddingBottom === 0 ? "0px" : currentPaddingBottom - remPaddingsVal + "px";
-    } else {
-      container.style.paddingBottom = currentPaddingBottom + remPaddingsVal + "px";
-      container.style.paddingTop = currentPaddingTop === 0 ? "0px" : currentPaddingTop - remPaddingsVal + "px";
-      
-    }
-  }
-
-  private topSentCallback = entry => {
-    if (this.currentIndex === 0) {
-      const container = this.shadowRoot.querySelector(".list") as HTMLElement;
-      container.style.paddingTop = "0px";
-      container.style.paddingBottom = "0px";
-    }
-
-    const currentY = entry.boundingClientRect.top;
-    const currentRatio = entry.intersectionRatio;
-    const isIntersecting = entry.isIntersecting;
-
-    // conditional check for Scrolling up
-    if (
-      currentY > this.topSentinelPreviousY &&
-      isIntersecting &&
-      this.currentRatio >= this.topSentinelPreviousRatio &&
-      currentIndex !== 0
-    ) {
-      const firstIndex = getSlidingWindow(false);
-      this.adjustPaddings(false);
-      this.recycleDOM(firstIndex);
-      currentIndex = firstIndex;
-    }
-
-    this.topSentinelPreviousY = currentY;
-    this.topSentinelPreviousRatio = currentRatio;
-  }
-
-  private bottomSentCallback = entry => {
-    if (this.currentIndex === this.collectionSize - this.listSize) {
-      return;
-    }
-    const currentY = entry.boundingClientRect.top;
-    const currentRatio = entry.intersectionRatio;
-    const isIntersecting = entry.isIntersecting;
-
-    // conditional check for Scrolling down
-    if (
-      currentY < this.bottomSentinelPreviousY &&
-      currentRatio > this.bottomSentinelPreviousRatio &&
-      isIntersecting
-    ) {
-      const firstIndex = getSlidingWindow(true);
-      this.adjustPaddings(true);
-      this.recycleDOM(firstIndex);
-      this.currentIndex = firstIndex;
-    }
-
-    this.bottomSentinelPreviousY = currentY;
-    this.bottomSentinelPreviousRatio = currentRatio;
-  };
-
-  initIntersectionObserver() {
-    const options = {
-      /* root: document.querySelector(".cat-list") */
-    }
-
-    const callback = entries => {
-      entries.forEach(entry => {
-        if (entry.target.id === 'tile-0') {
-          this.topSentCallback(entry);
-        } else if (entry.target.id === `tile-${this.listSize - 1}`) {
-          this.bottomSentCallback(entry);
-        }
-      });
-    }
-
-    var observer = new IntersectionObserver(callback, options);
-    observer.observe(document.querySelector("#tile-0"));
-    observer.observe(document.querySelector(`#tile-${this.listSize - 1}`));
-  }
 
   private init() {
-    this.collectionSize = 200;
-    this.listSize = 15;
-    this.collection = this.initCollection(this.collectionSize);
-    this.initList(listSize);
-    this.initIntersectionObserver();
+
   }
 
   render() {
     return html`
-      <div id="container">
-        <ul class="list" stle="top-padding: 0px; bottom-padding: 0px">
-        </ul>
-      </div>
+     
     `;
   }
 }
